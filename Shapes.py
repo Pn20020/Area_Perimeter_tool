d24ecd82e9371d236b055b8f25edd031b12eaa0b
Pn20020 <110356470+Pn20020@users.noreply.github.com>
2023-08-03 03:05:22 +0000

    Initial commit

---
+++ Shapes.py
@@ -0,0 +1,11 @@
+# show user list of shapes
+Shapes_list = ("circle", "square", "retangle",)
+print(Shapes_list)
+
+Shapes_list = input("Please choose a shape")
+
+
+
+print("You have choosen {}".format(Shapes_list))
+
+if inpu
---
+++ yes_no.py
@@ -0,0 +1,21 @@
+# functions go here 
+def yes_no(question):
+  valid = False
+  while not valid:
+    response = input(question).lower().strip() 
+    # input
+    if response == "yes" or response == "y":
+      response = "yes"
+      print("Display insturctions")
+      return response
+  
+    elif response == "no" or response == "n":
+      response = "no"
+      print("Program continues")
+      return response
+
+    else:
+      print("Please enter yes or no")
+
+
+question = yes_no("Do you want the instructions")
